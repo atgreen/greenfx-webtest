c645203e3deb711ecc50fe769ed12fe5433bb6ee
Anthony Green <green@moxielogic.com>
2017-02-15 14:15:48 -0500

    Show fx page

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 app.get('/', function (req, res) {
-  res.render('index.html', { pageCountMessage : null});
+  res.render('fx.html', { pageCountMessage : null});
 });
 
 // error handling
